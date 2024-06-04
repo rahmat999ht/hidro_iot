2d5337a2ba8b5c8ae59350cf9c974b035f592bb1
Iznsraaa <waisalqarni1712@gmail.com>
2024-06-05 06:58:40 +0800

    update icon dan data

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
+import 'package:hidroponik_iot/app/model/hidro.dart';
 import 'package:hidroponik_iot/component/button.dart';
 import 'package:hidroponik_iot/component/weather_detail.dart';
 import 'package:hidroponik_iot/theme/extension_context.dart';
@@ -14,218 +15,196 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const SizedBox(
-                width: 40,
-              ),
-              const Text('WELLCOME TO MY HIDROPONIC'),
-              ButtonKu(
-                  onTap: () {
-                    // Get.toNamed(Routes.PROFIL);
-                    context.goProfile();
-                  },
-                  child: Image.asset(
-                    'assets/img/logo.png',
-                    width: 40,
-                    height: 40,
-                  )),
-            ],
-          ),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(
+              width: 40,
+            ),
+            const Text('WELLCOME TO MY HIDROPONIC'),
+            ButtonKu(
+                onTap: () {
+                  // Get.toNamed(Routes.PROFIL);
+                  context.goProfile();
+                },
+                child: Image.asset(
+                  'assets/img/logo.png',
+                  width: 40,
+                  height: 40,
+                )),
+          ],
         ),
-        body: controller.obx(
-          (s) => ListView(
+      ),
+      body: controller.obx(
+        (s) {
+          final listData = [
+            ModelListHidro(
+              assets: 'assets/img/digital.png',
+              title: 'Digital',
+              value: s!.digital.toString(),
+            ),
+            ModelListHidro(
+              assets: 'assets/img/temperature.png',
+              title: 'Temperatur',
+              value: '${s.temperature}°',
+            ),
+            ModelListHidro(
+              assets: 'assets/img/ph.png',
+              title: 'Ph Matahari',
+              value: s.ph.toString(),
+            ),
+            ModelListHidro(
+              assets: 'assets/img/volt.png',
+              title: 'Voltage',
+              value: s.voltage.toString(),
+            ),
+          ];
+          return Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      color: Colors.grey.shade800,
-                      borderRadius: BorderRadius.circular(20)),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Row(
-                        children: [
-                          Icon(
-                            Icons.wb_cloudy,
-                            color: Colors.white,
-                            size: 50,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Berawan',
-                                style: TextStyle(
-                                    fontSize: 24,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.white),
-                              ),
-                              Text(
-                                'Makassar, Indonesia',
-                                style: TextStyle(fontSize: 20),
-                              )
-                            ],
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Text(
-                            '22°',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 60,
-                                fontWeight: FontWeight.bold),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          WeatherDetail(
-                              label: 'Digital', value: '${s?.digital}'),
-                          WeatherDetail(label: 'Ph', value: '${s?.ph}'),
-                          WeatherDetail(
-                              label: 'Temperature', value: '${s?.temperature}'),
-                          WeatherDetail(
-                              label: 'Voltage', value: '${s?.voltage}'),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              ),
+              cardCuaca(s),
               const SizedBox(
                 height: 20,
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Image.asset(
-                                  'assets/img/temperature.png',
-                                  height: 70,
-                                  width: 70,
-                                ),
-                                const Text(
-                                  'Digital',
-                                  style: TextStyle(
-                                      fontSize: 25,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Text('${s?.digital}')
-                              ],
-                            )),
-                      ),
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    ],
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      mainAxisSpacing: 16.0,
+                      crossAxisSpacing: 16.0,
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: listData.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final data = listData[index];
+                      return cardHidro(
+                        assets: data.assets,
+                        title: data.title,
+                        value: data.value,
+                      );
+                    },
                   ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                      Container(
-                        width: 180,
-                        height: 180,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: BorderRadius.circular(20)),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                ],
+                ),
+              ),
+            ],
+          );
+        },
+        onEmpty: const Text('kosong'),
+        onError: (e) => Text('error $e'),
+        onLoading: const CircularProgressIndicator(),
+      ),
+    );
+  }
+
+  Padding cardCuaca(HidroponicModel s) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Colors.grey.shade800,
+            borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Row(
+              children: [
+                Icon(
+                  Icons.wb_cloudy,
+                  color: Colors.white,
+                  size: 50,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Berawan',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      'Makassar, Indonesia',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  '22°',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                WeatherDetail(label: 'Digital', value: '${s.digital}'),
+                WeatherDetail(label: 'Ph', value: '${s.ph}'),
+                WeatherDetail(label: 'Temperature', value: '${s.temperature}'),
+                WeatherDetail(label: 'Voltage', value: '${s.voltage}'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container cardHidro({
+    required String assets,
+    required String title,
+    required String value,
+  }) {
+    return Container(
+      width: 180,
+      height: 180,
+      decoration: BoxDecoration(
+          color: Colors.grey.shade800, borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                assets,
+                height: 70,
+                width: 70,
+              ),
+              Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                value,
+                style: TextStyle(fontSize: 20),
               )
             ],
-          ),
-          onEmpty: const Text('kosong'),
-          onError: (e) => Text('error $e'),
-          onLoading: const CircularProgressIndicator(),
-        ));
+          )),
+    );
   }
+}
+
+class ModelListHidro {
+  final String assets;
+  final String title;
+  final String value;
+  ModelListHidro(
+      {required this.assets, required this.title, required this.value});
 }
